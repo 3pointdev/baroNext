c4f64340c636d058995fea8657a9130ad7d5483c
3pointdev <motors3996@gmail.com>
2023-08-16 10:40:23 +0900

    feat: deploy setting
    - 배포 환경 조성 완료

--- next.config.js
+++ next.config.js
@@ -1,20 +1,39 @@
-/** @type {import('next').NextConfig} */
-
-// 환경에 따라 다른 .env 파일 로드
-const dotenv = require("dotenv");
-const envFile =
-  process.env.NODE_ENV === "development"
-    ? ".env.development"
-    : ".env.production";
-
-dotenv.config({ path: envFile });
-console.log(envFile);
-
-const nextConfig = (phase) => {
-  return {
-    reactStrictMode: false,
-    swcMinify: true,
-  };
+const nextConfig = () => {
+  console.log(process.env.APP_ENV);
+  if (process.env.APP_ENV === "development") {
+    return {
+      env: {
+        NEXT_PUBLIC_BARO_URL:
+          "https://baroapi-dot-centered-sight-237801.appspot.com",
+        NEXT_PUBLIC_APIS_URL:
+          "https://apis-dot-centered-sight-237801.appspot.com",
+        NEXT_PUBLIC_EDGE_API_URL:
+          "https://edge-dot-centered-sight-237801.appspot.com",
+        NEXT_PUBLIC_WEBSOCKET_URL:
+          "wss://edge-dot-centered-sight-237801.appspot.com",
+        NEXT_PUBLIC_APP_MARK: "REACT_DEV",
+      },
+      reactStrictMode: false,
+      swcMinify: true,
+      output: "export",
+      distDir: "../build",
+    };
+  } else {
+    return {
+      env: {
+        NEXT_PUBLIC_BARO_URL: "https://baroapi-dot-prd-barofactory.appspot.com",
+        NEXT_PUBLIC_APIS_URL: "https://apis-dot-prd-barofactory.appspot.com",
+        NEXT_PUBLIC_EDGE_API_URL:
+          "https://edge-dot-prd-barofactory.appspot.com",
+        NEXT_PUBLIC_WEBSOCKET_URL: "wss://edge-dot-prd-barofactory.appspot.com",
+        NEXT_PUBLIC_APP_MARK: "REACT_PRD",
+      },
+      reactStrictMode: false,
+      swcMinify: true,
+      output: "export",
+      distDir: "../build",
+    };
+  }
 };
 
 module.exports = nextConfig;
