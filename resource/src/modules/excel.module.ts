56c6dc6287e88664ab956b8caa240ef83239d4e3
3pointdev <motors3996@gmail.com>
2023-09-08 14:27:09 +0900

    refactor : 1.0 이전 파일 삭제

--- resource/src/modules/excel.module.ts
+++ resource/src/modules/excel.module.ts
@@ -1,6 +1,5 @@
 import { RefObject } from "react";
-import { BookType } from "xlsx";
-import XLSXStyle from "xlsx-style";
+import XLSX, { BookType } from "xlsx";
 
 interface IExcelInfomation {
   targetRef: RefObject<HTMLTableElement>;
@@ -20,47 +19,25 @@ class ExcelModule {
     columnWidth = {},
     columnStyle,
   }: IExcelInfomation) {
-    //   const excelFile = XLSX.utils.table_to_book(targetRef.current, {
-    //     sheet: sheetName,
-    //     raw: true,
-    //   });
+    const excelFile = XLSX.utils.table_to_book(targetRef.current, {
+      sheet: sheetName,
+      raw: true,
+    });
 
-    //   excelFile.Sheets[sheetName]["!cols"] = Object.keys(columnWidth).map(
-    //     (key: string) => ({
-    //       wch: columnWidth[key],
-    //     })
-    //   );
+    excelFile.Sheets[sheetName]["!cols"] = Object.keys(columnWidth).map(
+      (key: string) => ({
+        wch: columnWidth[key],
+      })
+    );
 
-    //   console.log(excelFile);
+    XLSX.write(excelFile, {
+      bookType: fileType,
+      bookSST: true,
+      type: "base64",
+      ignoreEC: false,
+    });
 
-    //   for (
-    //     let i = 5;
-    //     i < Object.keys(excelFile.Sheets[sheetName]).length - 3;
-    //     i++
-    //   ) {
-    //     excelFile.Sheets[sheetName][
-    //       Object.keys(excelFile.Sheets[sheetName])[i]
-    //     ].s = { alignment: { horizontal: "center" } };
-    //   }
-
-    //   XLSX.write(excelFile, {
-    //     bookType: fileType,
-    //     bookSST: true,
-    //     type: "base64",
-    //     ignoreEC: false,
-    //   });
-
-    //   XLSX.writeFileXLSX(excelFile, fileName + fileType);
-
-    const workbook = XLSXStyle.readFile("template.xlsx"); // 기존 템플릿 파일 불러오기
-    const worksheet = workbook.Sheets["Sheet1"]; // 'Sheet1'에 해당하는 워크시트 선택
-    const cellStyle = {
-      fill: {
-        fgColor: { rgb: "FFFF00" }, // 노란색 배경색
-      },
-    };
-    worksheet["A1"].s = cellStyle;
-    XLSXStyle.writeFile(workbook, "output.xlsx");
+    XLSX.writeFileXLSX(excelFile, fileName + fileType);
   }
 }
 
