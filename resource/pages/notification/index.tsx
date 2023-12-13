50a1aef3ab717daa033f650de9c71f90d05de9ef
3pointdev <motors3996@gmail.com>
2023-12-13 15:09:45 +0900

    update: menu icon change
    - 메뉴 작업자관리, 공지사항관리 아이콘 변경

--- resource/pages/notification/index.tsx
+++ resource/pages/notification/index.tsx
@@ -1,10 +1,8 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Alert from "components/alert/alert";
 import DefaultButton from "components/button/defaultButton";
 import PageContainer from "components/container/pageContainer";
 import BigInput from "components/input/bigInput";
 import CardLayout from "components/layout/cardLayout";
-import { StyleColor } from "config/color";
 import { inject, observer } from "mobx-react";
 import { useEffect } from "react";
 import WorkerViewModel from "src/viewModels/worker/worker.viewModel";
@@ -62,70 +60,4 @@ const LayoutTitle = styled.h2`
   width: 100%;
   text-align: center;
   margin-bottom: 40px;
-`;
-
-const MachineWrap = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 24px;
-
-  @media screen and (max-width: 1258px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
-
-  @media screen and (max-width: 950px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-`;
-
-const MachineItem = styled.div`
-  position: relative;
-  box-shadow: ${StyleColor.BOXSHADOW};
-  background: ${StyleColor.EMPHASIS};
-  display: flex;
-  align-items: center;
-  width: calc(100% - 16px);
-  height: 64px;
-  padding: 8px;
-  gap: 32px;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 1;
-
-  & > div {
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-  }
-`;
-
-const MachineNumber = styled.p`
-  background: ${StyleColor.BRIGHTEMPHASIS};
-  width: 72px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const IconBox = styled.div`
-  cursor: pointer;
-  transition: all 0.4s;
-  border-radius: 50%;
-  position: absolute;
-  right: 8px;
-  top: 8px;
-  width: 26px;
-  height: 26px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  &:hover {
-    background: ${StyleColor.DARK}20;
-  }
-`;
-const EditIcon = styled(FontAwesomeIcon)`
-  width: 16px;
-  height: 16px;
 `;
